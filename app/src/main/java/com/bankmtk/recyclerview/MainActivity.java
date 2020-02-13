19707e7e8609c784a51f03b21cdcb4524acb58c8
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-02-13 22:12:02 +0300

    add RecyclerView, LinearLayoutManager

--- app/src/main/java/com/bankmtk/recyclerview/MainActivity.java
+++ app/src/main/java/com/bankmtk/recyclerview/MainActivity.java
@@ -1,6 +1,7 @@
 package com.bankmtk.recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -12,6 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+
+        //This setting is used to improve system performance
+        recyclerView.setHasFixedSize(true);
+
+        //we'll be work with integral manager
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        //setup adapter
+
+
 
 
     }
