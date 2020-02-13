acf2ff96f3f8aecaf0e75aeb3aacc2c8bebf106f
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-02-13 09:17:19 +0300

    add data

--- app/src/main/java/com/bankmtk/recyclerview/MainActivity.java
+++ app/src/main/java/com/bankmtk/recyclerview/MainActivity.java
@@ -1,14 +1,18 @@
 package com.bankmtk.recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private String[] data = {"One", "Two","Three","Four"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
